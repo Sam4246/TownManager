749516c83477bef3d3caa59cdaf5e664170ebe94
samdinan@gmail.com <samdinan@gmail.com>
2018-03-19 19:23:12 -0300

    Initial commit

---
+++ app/src/main/java/com/battleofswords/dnd/townmanager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.battleofswords.dnd.townmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
